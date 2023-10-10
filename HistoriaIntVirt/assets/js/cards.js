766fe89e14b0684e497e6c4211996d88e8e36c86
v4ndre <103652070+v4ndre@users.noreply.github.com>
2023-10-10 17:41:27 -0300

    Add files via upload

---
+++ HistoriaIntVirt/assets/js/cards.js
@@ -0,0 +1,53 @@
+function showInfo() {
+    document.getElementById('box_info').classList.add('show');
+}
+function unShowInfo() {
+    document.getElementById('box_info').classList.remove('show');
+}
+
+// 2
+
+function showInfo2() {
+    document.getElementById('box_info2').classList.add('show');
+}
+function unShowInfo2() {
+    document.getElementById('box_info2').classList.remove('show');
+}
+
+// 3
+
+function showInfo3() {
+    document.getElementById('box_info3').classList.add('show');
+}
+function unShowInfo3() {
+    document.getElementById('box_info3').classList.remove('show');
+}
+
+// 4
+
+function showInfo4() {
+    document.getElementById('box_info4').classList.add('show');
+}
+function unShowInfo4() {
+    document.getElementById('box_info4').classList.remove('show');
+}
+
+// 5
+
+function showInfo5() {
+    document.getElementById('box_info5').classList.add('show');
+}
+function unShowInfo5() {
+    document.getElementById('box_info5').classList.remove('show');
+}
+
+// 6
+
+function showInfo6() {
+    document.getElementById('box_info6').classList.add('show');
+}
+function unShowInfo6() {
+    document.getElementById('box_info6').classList.remove('show');
+}
+
+// 
